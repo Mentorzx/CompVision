2ed28cce3927e2caf51eb21d4607106b03da3aa2
Alex Lira <mentorlira@gmail.com>
2025-02-04 17:02:16 -0300

    Add some docstrings

--- src/factory.py
+++ src/factory.py
@@ -2,6 +2,22 @@ import cv2
 
 
 class VideoCaptureFactory:
+    """
+    Factory for creating video capture objects.
+
+    This class encapsulates the creation of a cv2.VideoCapture instance,
+    abstracting the details of video file handling.
+    """
+
     @staticmethod
     def create(source: str) -> cv2.VideoCapture:
+        """
+        Create and return a VideoCapture object for the given video source.
+
+        Args:
+            source (str): The path to the video file.
+
+        Returns:
+            cv2.VideoCapture: The video capture object.
+        """
         return cv2.VideoCapture(source)
--- src/singleton.py
+++ src/singleton.py
@@ -5,6 +5,12 @@ import numpy as np
 
 
 class VideoWriterSingleton:
+    """
+    Singleton class for writing video frames to a file.
+
+    This class ensures that only one video writer instance exists during the execution.
+    """
+
     _instance: Optional["VideoWriterSingleton"] = None
 
     def __new__(cls, filename: str, fps: int) -> "VideoWriterSingleton":
@@ -14,12 +20,21 @@ class VideoWriterSingleton:
         return cls._instance
 
     def _initialize(self, filename: str, fps: int) -> None:
+        """
+        Initialize the video writer.
+        """
         self.writer = imageio.get_writer(
             filename, fps=fps, codec="libx264", format="ffmpeg"
         )
 
     def append_frame(self, frame: np.ndarray) -> None:
+        """
+        Append a frame to the video.
+        """
         self.writer.append_data(frame)
 
     def close(self) -> None:
+        """
+        Finalize and close the video writer.
+        """
         self.writer.close()
